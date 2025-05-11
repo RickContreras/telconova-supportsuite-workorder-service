88a17cb96534ac135c19747e007a6c0cbdfc2fcf
Ricardo Contreras Garzón <102176435+RickContreras@users.noreply.github.com>
2025-05-11 01:15:33 +0000

    🔧✨ Corrige paquetes y configura seguridad; agrega dependencias necesarias
    - 🛠️ Corrige el paquete en `HelloWorldController` y `SecurityConfig`.
    - 🔒 Habilita la configuración de seguridad en `SecurityConfig`.
    - ➕ Agrega dependencias necesarias en `pom.xml`:
      - `spring-boot-starter-security`
      - `spring-security-oauth2-resource-server`
      - Otras dependencias relacionadas con seguridad y funcionalidad.

--- src/main/java/com/telconova/support_backend/config/SecurityConfig.java
+++ src/main/java/com/telconova/support_backend/config/SecurityConfig.java
@@ -1,10 +1,10 @@
-package com.telconova.supportsuite.config;
+package com.telconova.support_backend.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-//import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-//import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.filter.CorsFilter;
@@ -12,10 +12,10 @@ import org.springframework.web.filter.CorsFilter;
 import java.util.List;
 
 @Configuration  
-//@EnableWebSecurity  
+@EnableWebSecurity  
 public class SecurityConfig {  
-    //@Bean  
-    /*public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {  
+    @Bean  
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {  
         http  
             .csrf().disable()
             .cors().and()  // Habilitar CORS
@@ -26,7 +26,7 @@ public class SecurityConfig {
             .httpBasic();  
         return http.build();  
     }  
-*/
+
     @Bean
     public CorsFilter corsFilter() {
         CorsConfiguration config = new CorsConfiguration();
