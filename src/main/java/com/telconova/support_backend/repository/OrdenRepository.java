0111b32302eecb983149846808d6eb5e13f03a32
Sebastian Renteria <sebastian.renteria@udea.edu.co>
2025-05-13 01:13:19 +0000

    ✨ feat(orden): Implementa CRUD y filtrado de órdenes; agrega exportación a CSV

--- src/main/java/com/telconova/support_backend/repository/OrdenRepository.java
+++ src/main/java/com/telconova/support_backend/repository/OrdenRepository.java
@@ -1,5 +1,14 @@
 package com.telconova.support_backend.repository;
 
-public class OrdenRepository {
+import com.telconova.support_backend.entity.Orden;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
+public interface OrdenRepository extends JpaRepository<Orden, Long> {
+    // List<Orden> findByEstado(String estado);
+    // Buscar por estado (nombre), clienteId o tipoId
+    java.util.List<Orden> findByEstado_Nombre(String nombre);
+    java.util.List<Orden> findByCliente_Id(Long id);
+    java.util.List<Orden> findByTipo_Id(Long id);
 }
